4f49352aee4f5a97a6f5c90c03b6f929b26fd6fb
Neil Wadden <neil.a.wadden@mun.ca>
2024-10-13 20:39:24 -0230

    removed downloads

--- configuration.nix
+++ configuration.nix
@@ -60,12 +60,10 @@
     # Create and set ownership/permissions for Sonarr directories
     "d /home/neil/radarr/config 0755 1001 1000 -"
     "d /home/neil/media/tv 0755 1001 1000 -"
-    "d /path/to/downloads 0755 1001 1000 -"
     
     # Create and set ownership/permissions for Radarr directories
     "d /home/neil/sonarr/config 0755 1000 1000 -"
     "d /home/neil/media/movies 0755 1000 1000 -"
-    "d /path/to/downloads 0755 1000 1000 -"
   ];
 
 
