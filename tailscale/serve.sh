809384e41a9d7446f46f3030e38181ae9b276e09
Neil Wadden <neil.a.wadden@mun.ca>
2024-10-14 16:17:07 -0230

    another tweak to the serve script

--- tailscale/serve.sh
+++ tailscale/serve.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-tailscale serve -bg --http=5555 http://zapdos.lab:8096
-tailscale serve -bg --http=6666 http://zapdos.lab:5055
+tailscale serve -bg --http=5555 http://localhost:8096
+tailscale serve -bg --http=6666 http://localhost:5055
