4ad848d5022b69e062acb5a405cf9b3fe05c4648
Neil Wadden <neil.a.wadden@mun.ca>
2024-10-17 19:17:29 -0230

    allow udp packets

--- configuration.nix
+++ configuration.nix
@@ -29,7 +29,7 @@
   home-manager.users.neil = import ./home-manager/home.nix;
 
   networking.firewall.allowedTCPPorts = [ 4646 8096 8080 7878 5055 8989 9696 ];
-  networking.firewall.allowedUDPPorts = [ 8096 ];
+  networking.firewall.allowedUDPPorts = [ 8096 41641 ];
 
   users.groups.media = {
     gid = 1000;
