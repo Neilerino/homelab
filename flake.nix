f9d4a42fa060c9c8f5b90e043c60dc4a1d8befd0
Neil Wadden <neil.a.wadden@mun.ca>
2024-10-26 18:47:36 -0230

    change box name to zapdos

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     # Please replace my-nixos with your hostname
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.zapdos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
