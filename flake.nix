31baebdfc27b60b5142607e4223a06f8415fd850
Neil Wadden <naw218@mun.ca>
2026-03-01 15:48:46 -0330

    bump

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
